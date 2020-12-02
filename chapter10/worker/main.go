357daf6dec690d8e2275adb5e10546a568093728
chenjiefeng <chenjiefeng@ad61v1.com>
2020-12-02 15:05:55 +0800

    producer consumer worker

--- chapter10/worker/main.go
+++ chapter10/worker/main.go
@@ -4,22 +4,28 @@ import (
 	"fmt"
 	"learning/chapter10/worker/pool"
 	"sync"
+	"time"
 )
 
 func main() {
 	var wg sync.WaitGroup
+	var wgAddTask sync.WaitGroup
 
-	worker := pool.NewPool(5)
-	worker.AddTask(func() interface{} {
-		return 1
-	})
-	worker.AddTask(func() interface{} {
-		return 2
-	})
-	worker.AddTask(func() interface{} {
-		return 3
-	})
+	worker := pool.NewPool(2)
 
+	// 添加任务
+	wgAddTask.Add(1)
+	go func() {
+		for i := 0; i < 10; i++ {
+			worker.AddTask(func() interface{} {
+				return i
+			})
+			time.Sleep(time.Millisecond)
+		}
+		wgAddTask.Done()
+	}()
+
+	// 执行任务
 	worker.Start()
 
 	// 获取结果
@@ -31,9 +37,12 @@ func main() {
 		wg.Done()
 	}()
 
-	// 先关闭任务池
+	// 等待添加任务的例程结束
+	wgAddTask.Wait()
+
+	// 关闭任务池，等待任务执行完成
 	worker.Close()
 
-	// 再关闭获取结果的例程
+	// 再等待获取结果的例程结束
 	wg.Wait()
 }
--- chapter10/worker/pool/pool.go
+++ chapter10/worker/pool/pool.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"log"
 	"math"
 	"sync"
 )
@@ -25,18 +26,18 @@ func NewPool(worker int) *Pool {
 	}
 }
 
-// 向任务池添加任务
+// 向任务池添加任务，并告诉通知管道
 func (p *Pool) AddTask(task Task) {
 	p.tasks.Append(task)
 	// 通知任务池执行任务
 	p.events <- struct{}{}
 }
 
-// 执行任务
+// 遍历通知管道，有通知则从队列中取出任务并执行
 func (p *Pool) Start() {
 	for i := 0; i < p.worker; i++ {
 		p.wg.Add(1)
-		go func() {
+		go func(i int) {
 			// 遍历通知管道
 			for range p.events {
 				// 从队列中读取任务
@@ -44,6 +45,9 @@ func (p *Pool) Start() {
 				if err != nil {
 					continue
 				}
+
+				log.Printf("worker %d run task", i)
+
 				// 将队列中的空接口数据转换成Task并执行
 				if task, ok := e.(Task); ok {
 					// 将结果放入结果管道
@@ -51,11 +55,11 @@ func (p *Pool) Start() {
 				}
 			}
 			p.wg.Done()
-		}()
+		}(i)
 	}
 }
 
-// 关闭任务池
+// 关闭任务池，等待任务执行完成
 func (p *Pool) Close() {
 	close(p.events)
 	p.wg.Wait()
